8ad174597f1e37bf075412306c9e049508dd41dc
Josh Hadden <1126.jbird@gmail.com>
2021-03-13 12:12:00 -0500

    Awaiting Await

---
+++ WindowsFormsApp/WindowsFormsApp/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Firebase;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace WindowsFormsApp
+{
+    class Database
+    {
+        FirebaseClient firebase = new FirebaseClient("https://cliques-b6c58-default-rtdb.firebaseio.com/");
+        HttpClient client = new HttpClient();
+        await firebase.Child("Hello").Child("World").PutAsync("and Scott");
+
+    }
+}
